1c38b76c31b62abcdf2c3f4bec2321a8d7fc2f7d
Isaac Yoon <ysaacioon@yahoo.com>
2017-05-05 18:53:29 -0700

    change environment port

--- server/index.js
+++ server/index.js
@@ -9,6 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/client','index.html'))
 })
 
-app.listen(3000, function () {
-  console.log('port listening on 3000!')
+
+app.listen(process.env.PORT || 3000
 })
